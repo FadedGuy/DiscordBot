bba037c8cb9f9a71e8a5cd53d155bf2fcbe81cd3
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2020-12-25 19:18:29 +0100

    Help

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: 'help',
     description: 'Lista de comandos disponibles de Lil Peep',
-    execute(message, args){
+    execute(message, args, commandsList){
         msg = "::: info\nLista de ==comandos== disponibles\n";
 
-        for(let comd of client.commands){
+        for(let comd of commandsList){
             console.log(comd);
         }
 
--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on('message', message => {
     } else if(command == 'frase'){
         client.commands.get('frase').execute(message, args);
     } else if(command == 'help'){
-        client.commands.get('help').execute(message, args);
+        client.commands.get('help').execute(message, args, client.commands);
     } else{
         message.channel.send("```\nComando no encontrado, prueba -help para ver la lista de comandos disponibles y que es lo que hacen```")
     }
