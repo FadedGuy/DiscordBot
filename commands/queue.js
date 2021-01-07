be3d78a4940f587f1152252acc1b680c7beda42b
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-01-07 18:12:21 +0100

    Adicion de queue

--- commands/queue.js
+++ commands/queue.js
@@ -5,8 +5,13 @@ module.exports = {
         if(!message.member.voice.channel){
             return message.channel.send("Tienes que estar en un canal de voz para usar este comando");
         }
-        if(serverQueue.songs.length >= 1){
-            return message.channel.send("Canciones en cola: ");
+        if(serverQueue.songs.length > 0){
+            let msg = "```css\n[Canciones en cola]\n";
+            for(let song of serverQueue.songs){
+                msg += (". -->" + song.title + "\n");
+            }
+            msg += "```";
+            return message.channel.send(msg);
         } else{
             return message.channel.send("No hay ninguna cancion en cola");
         }
